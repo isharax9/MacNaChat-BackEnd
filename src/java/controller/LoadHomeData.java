b594d885f1c5ebba5ace82ecae1cdd0eaccefa0f
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2024-10-02 16:56:57 +0530

    LoadHomeData done💯✅

---
+++ src/java/controller/LoadHomeData.java
@@ -0,0 +1,152 @@
+package controller;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import entity.Chat;
+import entity.User;
+import entity.User_Status;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.HibernateUtil;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+
+/**
+ *
+ * @author isharaLakshitha
+ */
+@WebServlet(name = "LoadHomeData", urlPatterns = {"/LoadHomeData"})
+public class LoadHomeData extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Gson gson = new Gson();
+
+        JsonObject responseJson = new JsonObject();
+        responseJson.addProperty("status", false);
+        responseJson.addProperty("message", "Unable to process your request");
+
+        try {
+
+            Session session = HibernateUtil.getSessionFactory().openSession();
+
+            //get user id from request parameters
+            String userId = request.getParameter("id");
+
+            //get user object
+            User user = (User) session.get(User.class, Integer.parseInt(userId));
+
+            //get user status = 1 (online)
+            User_Status user_Status = (User_Status) session.get(User_Status.class, 1);
+
+            //update user status
+            user.setUser_Status(user_Status);
+            session.update(user);
+
+            //get other users
+            Criteria criteria1 = session.createCriteria(User.class);
+            criteria1.add(Restrictions.ne("id", user.getId()));
+
+            List<User> otherUserList = criteria1.list();
+
+            //get other user one by one
+            JsonArray jsonChatArray = new JsonArray();
+
+            for (User otherUser : otherUserList) {
+
+                //get last conversation
+                Criteria criteria2 = session.createCriteria(Chat.class);
+                criteria2.add(Restrictions.or(
+                        Restrictions.and(
+                                Restrictions.eq("from_user", user),
+                                Restrictions.eq("to_user", otherUser)
+                        ),
+                        Restrictions.and(
+                                Restrictions.eq("from_user", otherUser),
+                                Restrictions.eq("to_user", user)
+                        )
+                ));
+                criteria2.addOrder(Order.desc("id"));
+                criteria2.setMaxResults(1);
+
+                //create chat item json to send frontend data
+                JsonObject chatItem = new JsonObject();
+                chatItem.addProperty("other_user_id", otherUser.getId());
+                chatItem.addProperty("other_user_mobile", otherUser.getMobile());
+                chatItem.addProperty("other_user_name", otherUser.getFirst_name() + " " + otherUser.getLast_name());
+                chatItem.addProperty("other_user_status", otherUser.getUser_Status().getId());  //online = 1 , offline = 2 
+
+//                String applicationPath = request.getServletContext().getRealPath("");
+//                String newApplicationPath = applicationPath.replace("build" + File.separator + "web", "web");
+                // Define the folder where images will be stored
+//                File folder = new File(newApplicationPath + File.separator + "AvatarImages");
+                // Define the path for the image file
+//                String avatarImagePath = folder.getAbsolutePath() + File.separator + mobile + ".png";
+                //check avatar image
+                String serverPath = request.getServletContext().getRealPath("");
+                String otherUserAvatarImagePath = serverPath + File.separator + "AvatarImages" + File.separator + otherUser.getMobile() + ".png";
+
+                File otherUserAvatarImageFile = new File(otherUserAvatarImagePath);
+
+                if (otherUserAvatarImageFile.exists()) {
+                    //avatar image found
+                    chatItem.addProperty("avatar_image_found", true);
+                } else {
+                    //avatar image not found
+                    chatItem.addProperty("avatar_image_found", false);
+                    chatItem.addProperty("other_user_avatar_letters", otherUser.getFirst_name().charAt(0) + "" + otherUser.getLast_name().charAt(0));
+                }
+
+                //get chat list
+                List<Chat> dbChatList = criteria2.list();
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy, MMM dd hh:mm a");
+
+                if (dbChatList.isEmpty()) {
+                    //no chat
+
+                    chatItem.addProperty("message", "Let's Start New Conversation");
+                    chatItem.addProperty("dateTime", dateFormat.format(user.getRegistered_date_time()));
+                    chatItem.addProperty("chat_status_id", 1); //seen = 1 , unseen = 2 
+
+                } else {
+                    //found last chat
+
+                    chatItem.addProperty("message", dbChatList.get(0).getMessage());
+                    chatItem.addProperty("dateTime", dateFormat.format(dbChatList.get(0).getDate_time()));
+                    chatItem.addProperty("chat_status_id", dbChatList.get(0).getChat_Status().getId());  //seen = 1 , unseen = 2 
+
+                }
+                //get last conversation
+                jsonChatArray.add(chatItem);
+
+            }
+
+            //send users
+            responseJson.addProperty("status", true);
+            responseJson.addProperty("message", "Success");
+
+            responseJson.add("user", gson.toJsonTree(user));
+            responseJson.add("jsonChatArray", gson.toJsonTree(jsonChatArray));
+
+            session.beginTransaction().commit();
+            session.close();
+
+        } catch (Exception e) {
+        }
+        
+        response.setContentType("application/json");
+        response.getWriter().write(gson.toJson(responseJson));
+    }
+
+}
